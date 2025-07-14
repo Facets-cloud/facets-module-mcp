d58c5b3014fb9bb0963cda79a8f25765bc827319
SatyendraGollaFacets <satyendra.gola@facets.cloud>
2025-07-14 17:31:15 +0530

    Add storage/ to .gitignore and remove from git tracking

--- examples/storage/s3/1.0/output.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  output_interfaces = {}
-  output_attributes = {
-    bucket_name   = aws_s3_bucket.this.id
-    bucket_arn    = aws_s3_bucket.this.arn
-    bucket_region = aws_s3_bucket.this.region
-  }
-}
